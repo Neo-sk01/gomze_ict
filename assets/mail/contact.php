51c8887b287781d215008f8373ac61f3c155f042
Neosk <neosekaleli@gmail.com>
2024-10-02 02:15:26 +0200

    final v1

--- assets/mail/contact.php
+++ assets/mail/contact.php
@@ -42,7 +42,7 @@ if(get_magic_quotes_gpc()) {
 // Example $address = "joe.doe@yourdomain.com";
 
 //$address = "example@themeforest.net";
-$address = "Info@gomze.co.za";
+$address = "Info@yourdomain.com";
 
 
 // Configuration option.
